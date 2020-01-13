5075e20648dfaec2d9b3c0b3986a8203d84987cd
yan_wuji <18874969913@139.com>
2020-01-13 20:53:17 +0800

    calculator

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main (String[]  args){
+        Scanner scanner = new Scanner(System.in);
+        double m = scanner.nextDouble( );
+        String x = scanner.next();
+        double n = scanner.nextDouble();
+        if (x.equals("*")){
+            System.out.println(m * n);
+        }else if(x.equals("/")){
+            System.out.println(m / n);
+        }else if(x.equals("+")){
+            System.out.println(m + n);
+        }else if(x.equals("-")){
+            System.out.println(m - n);
+        }
+    }
+
+}
